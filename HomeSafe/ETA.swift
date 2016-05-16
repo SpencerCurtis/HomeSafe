8ad5518ea3adc5adce745874700ded9ac715e48b
Spencer Curtis <spencercolecurtis@gmail.com>
2016-05-16 15:45:44 -0600

    Small additions.

--- HomeSafe/ETA.swift
+++ HomeSafe/ETA.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreLocation
 
-class ETA {
+class EstimatedTimeOfArrival {
     
     let ETA: NSDate
     let destinationLocation: CLLocation
---
+++ HomeSafe/NotificationController.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationController.swift
+//  HomeSafe
+//
+//  Created by Spencer Curtis on 5/16/16.
+//  Copyright © 2016 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotificationController {
+    
+    func scheduleLocalNotification(user: User, ETA: EstimatedTimeOfArrival) {
+        let notification = UILocalNotification()
+        notification.fireDate = ETA.ETA
+        notification.alertTitle = "\(user.name) is not in their safe location yet."
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+}
