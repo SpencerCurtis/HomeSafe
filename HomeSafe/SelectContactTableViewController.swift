5f8db2092890c99fe0296b9e052e96827b7b5799
Spencer Curtis <spencercolecurtis@gmail.com>
2016-05-17 14:55:04 -0600

    Added A ViewController from Map that wasn't added to the project for some reason.

--- HomeSafe/SelectContactTableViewController.swift
+++ HomeSafe/SelectContactTableViewController.swift
@@ -14,6 +14,7 @@ class SelectContactTableViewController: UITableViewController {
     
     var userContacts = [CNContact]()
     var contactStore = CNContactStore()
+    
     static let sharedInstance = SelectContactTableViewController()
     
     
