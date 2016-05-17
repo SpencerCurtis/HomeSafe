33f4c582bc66c4eb2a883348ab0ff8d22ecc8957
Brock Chambers <brockchamb@gmail.com>
2016-05-17 11:01:26 -0600

    Added Colors file and annotations now have the callout to the right ability. Still need to add UI

---
+++ HomeSafe/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  HomeSafe
+//
+//  Created by admin on 5/17/16.
+//  Copyright © 2016 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class Colors {
+    
+    let hunterOrange = UIColor(red: 0.988, green: 0.263, blue: 0.110, alpha: 1)
+}
