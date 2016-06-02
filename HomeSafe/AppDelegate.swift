ce94d898b7dd237791498fece431e0841f40a2aa
Spencer Curtis <spencercolecurtis@gmail.com>
2016-06-02 13:01:39 -0600

    ETA functions have been tweaked. Hopefully they work now.

--- HomeSafe/AppDelegate.swift
+++ HomeSafe/AppDelegate.swift
@@ -11,49 +11,56 @@ import CloudKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         let notificationTypes: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
         let pushNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
         application.registerUserNotificationSettings(pushNotificationSettings)
         application.registerForRemoteNotifications()
-
+        
         AppearanceController.initializeAppearance()
-//        CloudKitController.sharedController.addPhoneNumber(UserController.sharedController.currentUser!)
-//        CloudKitController.sharedController.fetchUserForPhoneNumber("18019952468") { (otherUser) in
-//            CloudKitController.sharedController.loadETAForUser(otherUser!, completion: { (eta) in
-//                CloudKitController.sharedController.setupSubscriptionForETA(eta)
-//            })
-//        }
-//        CloudKitController.sharedController.subscribeToUsersAddingCurrentUserToContactList(UserController.sharedController.currentUser!)
-//        CloudKitController.sharedController.addCurrentUserToOtherUsersContactList(UserController.sharedController.currentUser!, phoneNumber: "18019952468")
-
+        //        CloudKitController.sharedController.addPhoneNumber(UserController.sharedController.currentUser!)
+        //        CloudKitController.sharedController.fetchUserForPhoneNumber("18019952468") { (otherUser) in
+        //            CloudKitController.sharedController.loadETAForUser(otherUser!, completion: { (eta) in
+        //                CloudKitController.sharedController.setupSubscriptionForETA(eta)
+        //            })
+        //        }
+        //        CloudKitController.sharedController.subscribeToUsersAddingCurrentUserToContactList(UserController.sharedController.currentUser!)
+        //        CloudKitController.sharedController.addCurrentUserToOtherUsersContactList(UserController.sharedController.currentUser!, phoneNumber: "18019952468")
+        
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        
+        if let currentUser = UserController.sharedController.currentUser {
+            CloudKitController.sharedController.checkForNewETA(currentUser, completion: {
+                let phoneNumberArray = NSUserDefaults.standardUserDefaults().valueForKey("phoneNumberArrayForETA") as! [String]
+                for phoneNumber in phoneNumberArray {
+                CloudKitController.sharedController.fetchETAAndSubscribe(phoneNumber)
+                }
+            })
+        }
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
@@ -61,12 +68,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
         print(userInfo)
         
-//        if let currentUser = UserController.sharedController.currentUser {
-//            print(userInfo)
-//            CloudKitController.sharedController.checkForNewContacts(currentUser)
-//        }
+        //        if let currentUser = UserController.sharedController.currentUser {
+        //            print(userInfo)
+        //            CloudKitController.sharedController.checkForNewContacts(currentUser)
+        //        }
     }
-
-
+    
+    
 }
 
